c38aa6b00fe42521170047c9028b43ba1696c23c
Rafa Marques <RafaMarques179@googlemail.com>
2011-07-04 21:15:24 +0200

    Connect Datei

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+	// Variablen
+	$host = "localhost";
+	$name = "root";
+	$dbpass = "";
+	$dbname = "Karmintalender";
+	// Verbindung aufbauen
+	$db = new mysqli($host, $name, $dbpass, $dbname);
+?>
